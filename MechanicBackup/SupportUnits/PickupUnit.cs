106cd39cb0cbb5d40a83e3b13c6c55850323263d
craftycram <github@craftycram.net>
2020-08-27 20:08:28 +0200

    fixed player entering vehicle pickup

--- MechanicBackup/SupportUnits/PickupUnit.cs
+++ MechanicBackup/SupportUnits/PickupUnit.cs
@@ -41,7 +41,7 @@ namespace MechanicBackup.SupportUnits
             {
                 mechVehicle.SetPositionWithSnap(player.Character.GetOffsetPositionFront(-2f));
             }
-            Game.LocalPlayer.Character.Tasks.EnterVehicle(mechVehicle, -1).WaitForCompletion(30000);
+            player.Character.Tasks.EnterVehicle(mechVehicle, 0).WaitForCompletion(30000);
             //Game.LocalPlayer.Character.Tasks.EnterVehicle(mechVehicle, 10000, -1, EnterVehicleFlags.None).WaitForCompletion(30000);
             //player.Character.WarpIntoVehicle(mechVehicle, 0);
             while (player.Character.CurrentVehicle != mechVehicle)
