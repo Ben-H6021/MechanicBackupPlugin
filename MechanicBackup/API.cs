d976a3f178e6b457d1d4af9297933eaf0c168520
craftycram <github@craftycram.net>
2020-09-19 05:23:13 +0200

    added api

---
+++ MechanicBackup/API.cs
@@ -0,0 +1,26 @@
+﻿using Rage;
+
+namespace MechanicBackup
+{
+    public static class API
+    {
+
+        public static void spawnMechanicUnit()
+        {
+            SupportUnits.MechanicUnit.spawn(Game.LocalPlayer);
+        }
+        public static void spawnTowingUnit()
+        {
+            SupportUnits.TowingUnit.spawn(Game.LocalPlayer);
+        }
+        public static void spawnPickupUnit()
+        {
+            SupportUnits.PickupUnit.spawn(Game.LocalPlayer);
+        }
+        public static void spawnDutyVehicleUnit()
+        {
+            SupportUnits.DutyVehicleUnit.spawn(Game.LocalPlayer);
+        }
+
+    }
+}
