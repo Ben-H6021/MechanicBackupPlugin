669201e2c616a536a2200e31c0e12ab0e7ffe819
Ben Hamilton <bchamilton@live.co.uk>
2022-02-20 16:38:25 +0000

    Extra

--- MechanicBackup/EntryPoint.cs
+++ MechanicBackup/EntryPoint.cs
@@ -19,9 +19,108 @@ namespace MechanicBackup
                 Game.DisplayNotification("MechanicBackupPlugin " + currentVersion + " loaded successfully");
                 Game.LogTrivial("MechanicBackupPlugin " + currentVersion + " loaded successfully");
 
+                try
+                {
+                    Thread FetchVersionThread = new Thread(() =>
+                    {
+
+                        using (WebClient client = new WebClient())
+                        {
+                            try
+                            {
+                                string s = client.DownloadString("http://www.lcpdfr.com/applications/downloadsng/interface/api.php?do=checkForUpdates&fileId=29789&textOnly=1");                              // <= CHANGE FILE ID!!!!
+
+                                newVersion = new Version(s);
+                            }
+                            catch (Exception e) { Game.LogTrivial("MechanicBackupPlugin: LSPDFR Update API down. Aborting checks."); }
+                        }
+                    });
+                    FetchVersionThread.Start();
+                    while (FetchVersionThread.ThreadState != System.Threading.ThreadState.Stopped)
+                    {
+                        GameFiber.Yield();
+                    }
+
+                    // compare the versions  
+                    if (currentVersion.CompareTo(newVersion) < 0)
+                    {
+                        Game.LogTrivial("MechanicBackupPlugin: Update Available for MechanicBackupPlugin. Installed Version " + currentVersion + "New Version " + newVersion);
+                        Game.DisplayNotification("~g~Update Available~w~ for ~b~MechanicBackupPlugin~w~.\nInstalled Version: ~y~" + currentVersion + "\n~w~New Version~y~ " + newVersion);
+                    }
+                }
+                catch (System.Threading.ThreadAbortException e)
+                {
+                    Game.LogTrivial("MechanicBackupPlugin: Error while checking for updates.");
+                }
+                catch (Exception e)
+                {
+                    Game.LogTrivial("MechanicBackupPlugin: Error while checking for updates.");
+                }
+
                 Player player = Game.LocalPlayer;
                 Menu.createMenu();
+
+
+
+                while (true)
+                {
+                    GameFiber.Yield();
+                    if (Game.IsKeyDown(Config.MenuKey))
+                    {
+                        Menu.mainMenu.Visible = !Menu.mainMenu.Visible;
+                    }
+                    if (Game.IsKeyDown(System.Windows.Forms.Keys.NumPad5) && Config.Debug)
+                    {
+                        GameFiber.StartNew(delegate
+                        {
+                            GameFiber.Yield();
+                            testing(player);
+                        });
+                    }
+
+                    Menu._menuPool.ProcessMenus();
+                }
             });
+        }
+
+        public static void testing(Player player)
+        {
+
+            Vehicle playerVehicle = player.Character.CurrentVehicle != null ? player.Character.CurrentVehicle : player.Character.LastVehicle;
+            /*
+            VehicleDoor[] doors = playerVehicle.GetDoors();
+            foreach (VehicleDoor door in doors)
+            {
+                Game.LogTrivial(Convert.ToString(door.Index));
+                door.Close(false);
+            }
+            playerVehicle.GetDoors()[2].Close(false);
+
+            Ped driverMech = new Ped("s_m_y_xmech_01", playerVehicle.GetOffsetPositionFront(-55f), playerVehicle.Heading);
+            Vehicle mechVehicle = new Vehicle("mesa3", playerVehicle.GetOffsetPositionFront(-50f), playerVehicle.Heading);
+
+
+            driverMech.BlockPermanentEvents = true;
+            driverMech.IsPersistent = true;
+            driverMech.IsInvincible = true;
+            mechVehicle.IsPersistent = true;
+
+            driverMech.Tasks.EnterVehicle(mechVehicle, 10000, -1, EnterVehicleFlags.None).WaitForCompletion(30000);
+
+            driverMech.Tasks.ParkVehicle(mechVehicle, playerVehicle.GetOffsetPositionFront(-playerVehicle.Length - 1f), playerVehicle.Heading).WaitForCompletion(30000);
+            mechVehicle.IsEngineOn = false;
+            driverMech.Tasks.LeaveVehicle(LeaveVehicleFlags.None).WaitForCompletion(10000);
+            driverMech.ClearLastVehicle();
+
+
+            driverMech.IsPersistent = false;
+            mechVehicle.IsPersistent = false;
+            */
+            int index = Workshop.getNearestWorkshopIndex(playerVehicle.Position);
+            Blip blip = new Blip(Workshop.spawnLocations[index]);
+
+            return;
+
         }
     }
 }
